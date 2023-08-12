98410d9bd5bd6dfc4d1b4177b5616cc66ab6ec5b
spankyman125 <spankyman@live.ru>
2023-08-12 13:59:14 +0700

    Add userJoined event handle

--- src/rooms/rooms.emitter.service.ts
+++ src/rooms/rooms.emitter.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { User } from '@prisma/client';
+import { Prisma, User } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { SocketService } from 'src/socket/socket.service';
 
@@ -17,13 +17,42 @@ export class RoomsEmitterService {
     }
   }
 
+  async userJoined(userJoined: User, roomId: number, initiatorId: number) {
+    const initiatorSocket = this.socketService.getSocket(initiatorId);
+    const joinedUserSocket = this.socketService.getSocket(userJoined.id);
+    if (initiatorSocket && joinedUserSocket) {
+      //TODO: split into invite and receive invite events
+      const lastMessage = {
+        messages: {
+          take: 1,
+          orderBy: { createdAt: Prisma.SortOrder.desc },
+          include: {
+            user: {
+              select: { avatarUrl: true, id: true, pseudonym: true },
+            },
+          },
+        },
+      };
+      const roomJoined = await this.prismaService.room.findUniqueOrThrow({
+        where: { id: roomId },
+        include: lastMessage,
+      });
+      joinedUserSocket.join(`rooms_${roomId}`);
+      joinedUserSocket.emit(`rooms:invite-accepted`, roomJoined);
+      initiatorSocket
+        .to(`rooms_${roomId}`)
+        .emit('rooms:joined', { ...userJoined, roomId });
     }
   }
 
-  async userLeft(userLeft: User, roomId: number, userId: number) {
-    const socket = this.socketService.getSocket(userId);
-    if (socket) {
-      socket.to(`rooms_${roomId}`).emit('rooms:left', userLeft);
+  async userLeft(userLeft: User, roomId: number, initiatorId: number) {
+    const initiatorSocket = this.socketService.getSocket(initiatorId);
+    const leftedUserSocket = this.socketService.getSocket(userLeft.id);
+    if (initiatorSocket && leftedUserSocket) {
+      leftedUserSocket.leave(`rooms_${roomId}`);
+      initiatorSocket
+        .to(`rooms_${roomId}`)
+        .emit('rooms:left', { ...userLeft, roomId });
     }
   }
 }
