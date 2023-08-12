da1b7a4beaf640aa06e35d4beb93fd87089b84ef
spankyman125 <spankyman@live.ru>
2023-08-12 13:50:53 +0700

    Static files serve

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,25 @@
-import { HttpAdapterHost, NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { PrismaClientExceptionFilter } from './shared/filters/prisma.filter';
 import { ValidationPipe } from '@nestjs/common';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { json } from 'body-parser';
+import * as compression from 'compression';
+import { join } from 'path';
+import { AppModule } from './app.module';
+import { PrismaClientExceptionFilter } from './shared/filters/prisma.filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const { httpAdapter } = app.get(HttpAdapterHost);
+
+  if (process.env.NODE_ENV === 'dev')
+    app.useStaticAssets(join(__dirname, '..', '..', 'public'));
   app.setGlobalPrefix('api');
   app.enableCors();
+  app.use(json({ limit: '50mb' }));
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
+  app.use(compression());
   const config = new DocumentBuilder()
     .addBearerAuth()
     .setTitle('SCA_API')
@@ -16,9 +27,8 @@ async function bootstrap() {
     .setVersion('0.1')
     .addTag('sca')
     .build();
-  const { httpAdapter } = app.get(HttpAdapterHost);
-  app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
   const document = SwaggerModule.createDocument(app, config);
+
   SwaggerModule.setup('api/docs', app, document);
   await app.listen(30125);
 }
