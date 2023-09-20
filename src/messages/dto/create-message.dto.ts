05c5e8ec863337d7e9184cc36e63250d7d9d5b44
spankyman125 <spankyman@live.ru>
2023-09-20 13:22:02 +0600

    Allow empty messages

--- src/messages/dto/create-message.dto.ts
+++ src/messages/dto/create-message.dto.ts
@@ -1,9 +1,8 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsString } from 'class-validator';
 
 export class CreateMessageDto {
   @ApiProperty()
   @IsString()
-  @IsNotEmpty()
   content: string;
 }
